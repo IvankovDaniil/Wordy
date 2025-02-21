508e8c1ac8807860f2634757eb346b78b688f25e
IvankovDaniil <64897743+IvankovDaniil@users.noreply.github.com>
2025-02-21 23:44:17 +0700

    Initial Commit

---
+++ MemoryHelper/MemoryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryHelperApp.swift
+//  MemoryHelper
+//
+//  Created by Даниил Иваньков on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemoryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
